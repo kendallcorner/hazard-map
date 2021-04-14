751831dcdb73d43fed073dae5224b8e59e5d9036
Kendall Werts <kmwerts@gmail.com>
2021-04-13 23:18:33 -0500

    secrets

--- frontend/static/js/github.js
+++ frontend/static/js/github.js
@@ -1,6 +1,6 @@
 var script = document.createElement('script');
 
-SECRET_KEY = 'AIzaSyDjkgyUNU_rBD2v7hVb31aLzB6Gypi1LGw';
+SECRET_KEY = 'secret';
 
 script.setAttribute('src','https://maps.googleapis.com/maps/api/js?key='+SECRET_KEY+'&callback=initMap&libraries=places');
 
